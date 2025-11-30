60e3a18bd771f2b9ecdaa47638db2d369f791219
akumbom <akumbom5ma@gmail.com>
2025-11-30 09:53:02 +0100

    updated meal generation prompt

--- app/middleware/meal_plan_generation_middleware.py
+++ app/middleware/meal_plan_generation_middleware.py
@@ -8,10 +8,10 @@ logger = logging.getLogger(__name__)
 
 
 async def process_meal_plan_generation(payload: Request):
+    data = await payload.json()
     try:
-        data = await payload.json()
         result = await generate_meal_plan(payload)
         return ServerResponse(name= data, data=result, status="success")
     except Exception as e:
-        logger.error(f"Unexpected error from processing image: {e}")
+        logger.error(f"Unexpected error from processing meal plan {data}: {e}")
         raise HTTPException(status_code=500, detail=str(e))
